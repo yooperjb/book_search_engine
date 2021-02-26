601040ea702a52382d4d45f375c3b7212f40e1b0
yooperjb <jmb181@humboldt.edu>
2021-02-26 15:17:37 -0800

    start apollo server setup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
+// require mongodb connection
 const db = require('./config/connection');
+// don't think we'll need these
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -16,6 +19,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+// looks for db to open - upon successfully connection, start the server
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
